ba67cfa40f11327a6d50959db693e56464c60958
iq3 <iq3@addli.co.jp>
2017-02-06 20:00:08 +0900

    update RequestMapping.

--- src/main/java/jp/co/addli/controller/Controller.java
+++ src/main/java/jp/co/addli/controller/Controller.java
@@ -2,8 +2,10 @@ package jp.co.addli.controller;
 
 import jp.co.addli.model.UsersResponse;
 import jp.co.addli.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
  * Project:example_SpringBoot_multiDatasource Created by iq3 on 2017/02/06.
  */
 @RestController
-@RequestMapping(value = { "/master","/second" })
+@RequestMapping(value = "/{*}" )
+@Slf4j
 public class Controller {
 
     @Autowired
